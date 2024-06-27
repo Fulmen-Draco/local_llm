aa8aa9cc14e405d41b2d2e75d4f6d18487ca21cb
Aditya Sinha <92170969+Fulmen-Draco@users.noreply.github.com>
2024-06-27 13:33:17 +0530

    ollama to hf api

--- ollama.py
+++ ollama.py
@@ -1,9 +1,17 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
+from langchain_huggingface import HuggingFaceEndpoint
 import streamlit as st
 
-llm = ChatOllama(model="gemma:2b",temperature=0.5)
+repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
+
+llm = HuggingFaceEndpoint(
+    repo_id=repo_id,
+    max_new_tokens=512,
+    temperature=0.5,
+    huggingfacehub_api_token="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",
+)
 prompt = ChatPromptTemplate.from_messages([
     ("system","You are a proficient {Language} Programmer, and will now assist me with by solving my doubts."),
     ("user","{query}")
